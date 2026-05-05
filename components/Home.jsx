745e37b99a5c183193698f99d47b9d4817181a9c
Dogbonee <hundeknochen57@gmail.com>
2026-05-04 18:28:21 -0600

    Fix typo

--- components/Home.jsx
+++ components/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
             <div className="w-full md:w-1/2 flex flex-col justify-center text-center md:text-left">
                 <h1 className="text-4xl md:text-5xl font-bold mb-4">About me</h1>
                 <p className="text-xl md:text-2xl">
-                    I am a second-year Computer Engineering studying at the South Dakota
+                    I am a second-year Computer Engineering student studying at the South Dakota
                     School of Mines and Technology. Confident in app development with frameworks
                     such as Flutter as well as embedded systems.
                 </p>
